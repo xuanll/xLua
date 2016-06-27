4a12edfa860d5f9a2e502ef040d79bb23e93bb1c
Antonis <linxuan.lv@kunlun-inc.com>
2016-06-27 10:05:46 +0800

    update ClassTemplate

--- Codes/Template/ClassTemplate.lua
+++ Codes/Template/ClassTemplate.lua
@@ -1,4 +1,9 @@
--- 第二个参数是父类
-class XClass = ("XClass", require("GUI.BaseUIX"))
+-- 基础写法
+class BaseUIX = ("BaseUIX")
+
+return BaseUIX
+
+-- 含有继承的写法，第二个参数是父类
+class XClass = ("XClass", require("BaseUIX"))
 
 return XClass
