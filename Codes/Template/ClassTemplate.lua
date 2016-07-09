c1bf8d3981adaf62b691ccd2cd59a0751abbbf39
Antonis <linxuan.lv@kunlun-inc.com>
2016-07-09 17:59:03 +0800

    update ClassTemplate - add call father class method

--- Codes/Template/ClassTemplate.lua
+++ Codes/Template/ClassTemplate.lua
@@ -1,9 +1,26 @@
 -- 基础写法
+```lua
 class BaseUIX = ("BaseUIX")
+BaseUIX.p = nil
+
+function BaseUIX:Fn(value)
+    -- do sth
+end
 
 return BaseUIX
+```
 
 -- 含有继承的写法，第二个参数是父类
+```lua
 class XClass = ("XClass", require("BaseUIX"))
+local BaseUIX = require("BaseUIX")
+
+function XClass:Fn(value)
+    -- 调用父类方法
+    BaseUIX.Fn(self, value)
+
+    -- do sth else
+end
 
 return XClass
+```
